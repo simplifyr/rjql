fcef4bef7777d0b7abbeb0f823e3dfa022fff57c
rahulnjs <rahulsharmaa@hotmail.com>
2020-07-14 00:58:40 +0530

    updated build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm dist -r
 npm run build
 cp dist/rjql.min.js ../rjql-doc/public/rjql.min.js
-cp dist/rjql.min.js ../simplifier/public/rjql.min.js
-cp dist/rjql.js ../simplifier-backend/lib/validators/rjql.js
+cp dist/rjql.min.js ../simplifyr/public/rjql.js
+cp dist/rjql.js ../simplifyr-backend/lib/validators/rjql.js
